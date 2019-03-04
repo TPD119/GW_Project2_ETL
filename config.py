40a533ca848f2a6a148c57d6a39df4f6bdddf9df
tpd119 <thomasdirienzo@gmail.com>
2019-03-04 07:26:04 -0500

    added moving violations, i need to add config import

---
+++ config.py
@@ -0,0 +1,19 @@
+# AWS Database Info
+remote_db_endpoint = 'gwcodingbootcamp.cr0gccbv4ylw.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_gwsis_dbname = 'gwcodingbootcamp'
+remote_gwsis_dbuser = 'root'
+remote_gwsis_dbpwd = 'braddocks'
+
+# local Database Info
+local_gwsis_dbname = 'gwsis'
+local_gwsis_dbuser = 'root'
+local_gwsis_dbpwd = '<ce9rJBdqqc+(>'
+
+# Salesforce 
+sf_grant_type = 'password'
+sf_client_id = '' # Consumer Key
+sf_client_secret = '' # Consumer Secret
+sf_username = '' # The email you use to login
+sf_password = '' # Concat your password and your security token
+sf_endpoint = 'https://login.salesforce.com/services/oauth2/token'
