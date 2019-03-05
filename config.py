31fc742434523e0141db90a68630656554745029
tpd119 <thomasdirienzo@gmail.com>
2019-03-05 14:39:22 -0500

    added config and AWS link, need pwds to run

--- config.py
+++ config.py
@@ -3,17 +3,11 @@ remote_db_endpoint = 'gwcodingbootcamp.cr0gccbv4ylw.us-east-2.rds.amazonaws.com'
 remote_db_port = '3306'
 remote_gwsis_dbname = 'gwcodingbootcamp'
 remote_gwsis_dbuser = 'root'
-remote_gwsis_dbpwd = 'braddocks'
+# ADD PASSWORD HERE FOR AWS DATABASE
+remote_gwsis_dbpwd = ''
 
 # local Database Info
 local_parkingmoving_dbname = 'parkingmovingdc'
 local_parkingmoving_dbuser = 'root'
+###ADD PASSWORD HERE FOR SQL WORKBENCH
 local_parkingmoving_dbpwd = ''
-
-# Salesforce 
-sf_grant_type = 'password'
-sf_client_id = '' # Consumer Key
-sf_client_secret = '' # Consumer Secret
-sf_username = '' # The email you use to login
-sf_password = '' # Concat your password and your security token
-sf_endpoint = 'https://login.salesforce.com/services/oauth2/token'
