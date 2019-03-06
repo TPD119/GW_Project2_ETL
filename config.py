eb23339aa16a9570f8e1b321b4dfde555f22f2a9
tpd119 <thomasdirienzo@gmail.com>
2019-03-06 00:00:26 -0500

    aws added

--- config.py
+++ config.py
@@ -4,10 +4,6 @@ remote_db_port = '3306'
 remote_gwsis_dbname = 'gwcodingbootcamp'
 remote_gwsis_dbuser = 'root'
 # ADD PASSWORD HERE FOR AWS DATABASE
-remote_gwsis_dbpwd = ''
+remote_gwsis_dbpwd = 'braddocks'
 
-# local Database Info
-local_parkingmoving_dbname = 'parkingmovingdc'
-local_parkingmoving_dbuser = 'root'
-###ADD PASSWORD HERE FOR SQL WORKBENCH
-local_parkingmoving_dbpwd = ''
+
